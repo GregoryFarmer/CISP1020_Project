f659f7d0851c6767001b04514f73475076fc6e67
GregoryFarmer <gregorymfarmer@gmail.com>
2026-03-12 19:16:11 -0400

    Updated Car class

--- src/main/java/com/cisp1020/cisp1020_project/Car.java
+++ src/main/java/com/cisp1020/cisp1020_project/Car.java
@@ -9,15 +9,21 @@ package com.cisp1020.cisp1020_project;
  * @author Gregory Farmer <gregory.farmer>
  */
 public class Car {
-    public static int cars = 0;
-    private int id;
+    private static int cars = 0;
+    public int id;
+    public String name;
     
-    public Car() {
+    public Car(String name) {
         this.id = cars;
+        this.name = name;
         id++;
     }
     
     public int getID() {
         return this.id;
+    }
+    
+    @Override public String toString() {
+        return String.format("Car name: %s\nCar id: %s", this.name, this.id);
     }
 }
