51e1abed784ed5ac6172b68c0ed02600794c4f14
GregoryFarmer <gregorymfarmer@gmail.com>
2026-03-12 18:38:33 -0400

    Initial files

---
+++ src/main/java/com/cisp1020/cisp1020_project/CISP1020_Project.java
@@ -0,0 +1,13 @@
+package com.cisp1020.cisp1020_project;
+
+/**
+ *
+ * @author Gregory Farmer <GregoryFarmer>, Jacob Hidinger <StronGeek>, Ryan McClure, Zemetrik Ellison
+ * 
+ */
+public class CISP1020_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
