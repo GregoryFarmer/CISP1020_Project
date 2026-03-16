5afaec6c1993841727431dd3d0d1d9222e9b00eb
GregoryFarmer <gregorymfarmer@gmail.com>
2026-03-16 12:43:53 -0400

    Test

--- src/main/java/com/cisp1020/cisp1020_project/Car.java
+++ src/main/java/com/cisp1020/cisp1020_project/Car.java
@@ -1,24 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.cisp1020.cisp1020_project;
 
 /**
- *
+ * The Car superclass that creates a new car with a name.
+ * 
  * @author Gregory Farmer <gregory.farmer>
  */
 public class Car {
     private static int cars = 0;
     public int id;
     public String name;
-    
+   
+    /**
+     * 
+     * @param name 
+     */
     public Car(String name) {
         this.id = cars;
         this.name = name;
         id++;
     }
     
+    /**
+     * 
+     * @return The car's name
+     */
     public int getID() {
         return this.id;
     }
